40cbcb74350623b05395f05b21a90452558481ac
Gabi455 <90388665+Gabi4555@users.noreply.github.com>
2025-09-17 08:06:11 +0200

    sex

---
+++ Újvárosi Katona Gábriel/html/index.js
@@ -0,0 +1,24 @@
+
+
+
+function Textcolor(color) {
+
+
+
+    document.getElementById("Teszt_text").style.color = color;
+}
+
+function Textbackgoundcolor(color){
+    
+
+    document.getElementById("Teszt_text").style.backgroundColor = color;
+
+}
+
+function Fontsize(size) {
+    document.getElementById("Teszt_text").style.fontSize = size + "px";
+}
+
+function TextBorder(style) {
+    document.getElementById("Teszt_text").style.borderStyle = style;
+}
