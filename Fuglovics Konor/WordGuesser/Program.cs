3f0e3122779f1e1f1a84545cde8ee42d23afaafd
Sound Crafter <konor.fuglovics@gmail.com>
2025-11-06 15:10:21 +0100

    betű

---
+++ Fuglovics Konor/WordGuesser/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace WordGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = ["fuvola", "csirke", "adatok", "asztal", "fogoly", "bicska","farkas", "almafa", "babona", "gerinc", "dervis", "bagoly", "ecetes", "angyal", "boglya"];
+			Random rnd = new Random();
+			int num = rnd.Next(0,16);
+			int pos = num;
+			string identical = words[num];
+			string user = Console.ReadLine();
+			if(user.Length > 0)
+			{
+				for(int i = 0; i < identical.Length; i++)
+				{
+					if(user[i] == identical[i])
+					{
+						Console.WriteLine(user[i]);
+					}
+					else
+					{
+						Console.WriteLine(".");
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine("Nem írtál semmit!");
+			}
+			Console.ReadKey();
+        }
+    }
+}
